468899750a360a0961db4f6130faebe3a9f265f3
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2022-02-20 12:31:51 -0300

    Bando de dados H2 configurado e funcionando

---
+++ backend/src/main/java/com/scasistemas/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.scasistemas.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scasistemas.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository  extends JpaRepository<Category, Long> {
+
+}
