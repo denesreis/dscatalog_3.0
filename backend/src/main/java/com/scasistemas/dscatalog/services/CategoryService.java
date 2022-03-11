af0eff838cdfeb31d6a1425891cfca3f3506c7c5
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2022-03-11 08:12:52 -0300

    Category PUT

--- backend/src/main/java/com/scasistemas/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/scasistemas/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.scasistemas.dscatalog.dto.CategoryDTO;
 import com.scasistemas.dscatalog.entities.Category;
 import com.scasistemas.dscatalog.repositories.CategoryRepository;
-import com.scasistemas.dscatalog.services.exceptions.EntityNotFoundException;
+import com.scasistemas.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -32,12 +34,13 @@ public class CategoryService {
 	public CategoryDTO findById(Long id){
 		
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não localizada"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não localizada"));
 		
 		return new CategoryDTO(entity);
 		
 	}
-
+	
+	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
@@ -45,7 +48,24 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 		
 		
-	}	
-	
+	}
+	@Transactional
+	public CategoryDTO update(Long id,CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			repository.save(entity);
+			
+			return new CategoryDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException  ("Id not found: "+id);
+			
+		}
+			
+
+	}
+
+
 	
 }
