adfafe86931c707ee53cee049356afd937fc3613
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2022-03-14 09:58:51 -0300

    Dados de auditoria

--- backend/src/main/java/com/scasistemas/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/scasistemas/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,23 +9,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.scasistemas.dscatalog.services.exceptions.DatabaseException;
 import com.scasistemas.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e,HttpServletRequest request){
-	
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Recurso não encontrado");
 		err.setMessage(e.getMessage()); //Pegando a mensagem definida no service e que entrou no parametro da função
 		err.setPath(request.getRequestURI()); ///Para pegar o caminho da requisição
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err); //método status permite customizar o status que vai retornar | Retornando no corpo o objeto instanciado
-		
-		
+		return ResponseEntity.status(status).body(err); //método status permite customizar o status que vai retornar | Retornando no corpo o objeto instanciado
 		
 	}
 
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e,HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Recurso não encontrado");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI()); 
+		return ResponseEntity.status(status).body(err); 
+		
+	}
+	
+	
 }
